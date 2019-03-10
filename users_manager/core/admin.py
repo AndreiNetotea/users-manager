cf5e9f13a04e9c0dd7bd9b0840ba41f457ec2445
AndreiNetotea <netoteaandrei@gmail.com>
2019-03-10 13:57:20 +0200

    Create user model

--- users_manager/core/admin.py
+++ users_manager/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import UserProfile
 
 # Register your models here.
+
+# Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'updated_at', 'cv', 'image')
+    search_fields = ('id', 'name')
+
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
