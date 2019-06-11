8b0f8c298929d6fb8f91e90c7a07c3a23864ca48
AndreiNetotea <netoteaandrei@gmail.com>
2019-06-12 00:28:49 +0300

    Added some scripts

--- users_manager/settings.py
+++ users_manager/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'users_manager'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -139,14 +140,12 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(PROJECT_ROOT, "static")
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL = '/login'
